bf25633649a626c00ba7d0845e9827bfecad0147
Yan Sassi <yankingparts@gmail.com>
2026-01-09 20:33:50 -0300

    Fix ReferenceError in Layout.jsx

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -15,7 +15,7 @@ export default function Layout({ country, setCountry }) {
     <div className="flex h-screen bg-background-light dark:bg-background-dark text-gray-900 dark:text-white transition-colors duration-300 font-sans overflow-hidden">
       <Sidebar country={country} />
       <div className="flex-1 flex flex-col min-w-0">
-        <Header onToggleTheme={toggleTheme} country={country} setCountry={setCountry} isDarkMode={darkMode} />
+        <Header onToggleTheme={toggleDarkMode} country={country} setCountry={setCountry} isDarkMode={darkMode} />
         <main className="flex-1 overflow-y-auto p-4 md:p-8 custom-scrollbar relative">
           <Outlet context={{ country, setCountry }} />
         </main>
